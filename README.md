a0e80ce58dccdfb165c21ca5f675d1c6458b0add
Vlastik <vlastikskupien@seznam.cz>
2022-08-04 22:33:38 +0200

    feat: added data fetch from firebase

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Food Order app
 
-todo: fix item remove button in cart
-
 React app made with Vite to train React hooks
 
 ![vite logo](./src/favicon.svg)
+
+Backend in Firebase: https://console.firebase.google.com/project/food-order-app-fe4cd/database/food-order-app-fe4cd-default-rtdb/data/~2F
--- src/components/FoodList/index.jsx
+++ src/components/FoodList/index.jsx
@@ -1,23 +1,49 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import FoodItem from "./FoodItem"
 import "./FoodList.scss"
-import mealsData from "../../../data/meals.json"
 
 function FoodList() {
-	// const [foodData, setFoodData] = useState(null)
+	const [foodData, setFoodData] = useState(null)
+	const [isLoading, setIsLoading] = useState(true)
+	const [httpError, setHttpError] = useState(null)
 
-	// useEffect(() => {
-	// 	fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
-	// 		.then((res) => res.json())
-	// 		.then((data) => setFoodData(data.meals))
-	// }, [])
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(
+				`${
+					// @ts-ignore
+					import.meta.env.VITE_FIREBASE_ID
+				}`
+			)
+			if (!response.ok) {
+				throw new Error(response.statusText)
+			}
 
-	const { meals } = mealsData
+			const responseData = await response.json()
+
+			setFoodData(responseData)
+			setIsLoading(false)
+		}
+
+		fetchMeals().catch((error) => {
+			setHttpError(error.message)
+			setIsLoading(false)
+		})
+	}, [])
+
+	if (httpError) {
+		return <div className="meals-error">{httpError}</div>
+	}
 
 	return (
 		<div className="food-list">
-			{meals &&
-				meals.map((itemData) => <FoodItem data={itemData} key={itemData.id} />)}
+			{isLoading ? (
+				<div>Loading...</div>
+			) : (
+				foodData.map((itemData) => (
+					<FoodItem data={itemData} key={itemData.id} />
+				))
+			)}
 		</div>
 	)
 }
